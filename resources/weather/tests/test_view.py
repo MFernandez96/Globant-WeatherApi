b044d57979ddee529f8411e7ef7b825a0950fe0f
Martin Fernandez <mfernandez@seenka.com>
2022-08-12 01:18:01 -0300

    fix assert

--- resources/weather/tests/test_view.py
+++ resources/weather/tests/test_view.py
@@ -1,22 +1,19 @@
+import json
 from collections import namedtuple
 
 import pytest
 from resources.weather.views import Weather
-import json
 
 pytestmark = pytest.mark.django_db
 
 
 def test_weather_view():
-    import ipdb
-
-    ipdb.set_trace()
     request = namedtuple("Request", ["query_params"])
     request.query_params = {"city": "London", "country": "UK"}
     response = Weather().get(request)
     data = json.loads(response.content)
     assert response.__getitem__("Content-Type") == "application/json"
-    assert data.keys() == {
+    assert set(data.keys()) == {
         "location_name",
         "temperature",
         "wind",
