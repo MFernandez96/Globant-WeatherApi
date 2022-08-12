8c7f96839265a3aff22d2fbea9871779b9acda42
Martin Fernandez <mfernandez@seenka.com>
2022-08-12 01:18:27 -0300

    functional api

--- resources/weather/weatherman.py
+++ resources/weather/weatherman.py
@@ -2,44 +2,105 @@ import os
 from datetime import datetime
 
 import requests
+from beaufort_scale import beaufort_scale_ms
 from django.core.cache import cache
 
 
 class Weatherman(object):
     def __init__(self, city: str, country: str) -> None:
+        self.wind_utils = WindUtils()
+        self.temp_utils = TempUtils()
         self.city = city
         self.country = country
+        self.result = {}
 
     def get_current_weather(self) -> dict:
         self.setup_api_data()
-        return self.get_current_weather_data()
+        self.parse_weather_data()
+        self.parse_forecast_data()
+        return self.result
 
     def setup_api_data(self):
         self.retrieve_from_cache()
         if not self.weather or not self.forecast:
             self.get_from_api()
 
+    def retrieve_from_cache(self):
+        self.weather = cache.get("weather")
+        self.forecast = cache.get("forecast")
+
     def get_from_api(self):
-        self.weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={self.city},{self.country}&appid={os.getenv("OPENWEATHER_API_KEY")}'
-        self.forecast_url = f'http://api.openweathermap.org/data/2.5/forecast?q={self.city},{self.country}&appid={os.getenv("OPENWEATHER_API_KEY")}'
+        self.weather_url = f"""http://api.openweathermap.org/data/2.5/weather?q={self.city},{self.country}&appid={os.getenv("OPENWEATHER_API_KEY")}"""
+        self.forecast_url = f"""http://api.openweathermap.org/data/2.5/forecast?q={self.city},{self.country}&appid={os.getenv("OPENWEATHER_API_KEY")}"""
         self.weather = requests.get(self.weather_url).json()
         self.forecast = requests.get(self.forecast_url).json()
         cache.set("weather", self.weather, timeout=120)
         cache.set("forecast", self.forecast, timeout=120)
 
-    def retrieve_from_cache(self):
-        self.weather = cache.get("weather")
-        self.forecast = cache.get("forecast")
+    def parse_weather_data(self):
+        self.result["location_name"] = f"""{self.city}, {self.country.upper()}"""
+        self.result[
+            "temperature"
+        ] = f"""{self.temp_utils.kelvin_to_celsius(self.weather["main"]["temp"])} °C | {self.temp_utils.kelvin_to_fahrenheit(self.weather["main"]["temp"])} °F"""
+        self.result[
+            "wind"
+        ] = f"""{beaufort_scale_ms(self.weather["wind"]["speed"])}, {self.weather["wind"]["speed"]} m/s, {self.wind_utils.degrees_to_compass(self.weather["wind"]["deg"])}"""
+        self.result["cloudiness"] = self.weather["weather"][0]["description"]
+        self.result["pressure"] = f"""{self.weather["main"]["pressure"]} hPa"""
+        self.result["humidity"] = f"""{self.weather["main"]["humidity"]}%"""
+        self.result["sunrise"] = datetime.utcfromtimestamp(
+            self.weather["sys"]["sunrise"]
+        ).strftime("%H:%M")
+        self.result["sunset"] = datetime.utcfromtimestamp(
+            self.weather["sys"]["sunset"]
+        ).strftime("%H:%M")
+        self.result[
+            "geo_coordinates"
+        ] = f"""{self.weather["coord"]["lon"]}, {self.weather["coord"]["lat"]}"""
+        self.result["requested_time"] = datetime.utcfromtimestamp(
+            self.weather["dt"]
+        ).strftime("%Y-%m-%d %H:%M:%S")
 
-    def get_temperature_in_celsius(self, temperature):
-        temperature_in_celsius = int(temperature) - 273.15
-        return round(temperature_in_celsius, 2)
+    def parse_forecast_data(self):
+        self.result["forecast"] = {}
+        if self.forecast["cod"] == "200":
+            forecast_data = {}
+            for forecast in self.forecast["list"]:
+                forecast_time = forecast["dt_txt"]
+                forecast_data[forecast_time] = {
+                    "temperature": f"""{int(self.temp_utils.kelvin_to_celsius(forecast["main"]["temp"]))} °C | {int(self.temp_utils.kelvin_to_fahrenheit(forecast["main"]["temp"]))} °F"""
+                }
 
-    def get_temperature_in_fahrenheit(self, temperature):
-        temperature_in_fahrenheit = int(temperature) * 9 / 5 - 459.67
-        return round(temperature_in_fahrenheit, 2)
+                forecast_data[forecast_time][
+                    "wind"
+                ] = f"""{beaufort_scale_ms(forecast["wind"]["speed"])}, {forecast["wind"]["speed"]} m/s, {self.wind_utils.degrees_to_compass(forecast["wind"]["deg"])}"""
 
-    def degrees_to_compass(self, degrees):
+                forecast_data[forecast_time]["cloudiness"] = forecast["weather"][0][
+                    "description"
+                ]
+
+                forecast_data[forecast_time][
+                    "humidity"
+                ] = f"""{forecast["main"]["humidity"]}%"""
+
+                forecast_data[forecast_time]["sunrise"] = datetime.utcfromtimestamp(
+                    self.forecast["city"]["sunrise"]
+                ).strftime("%H:%M")
+
+                forecast_data[forecast_time]["sunset"] = datetime.utcfromtimestamp(
+                    self.forecast["city"]["sunset"]
+                ).strftime("%H:%M")
+
+                forecast_data[forecast_time][
+                    "geo_coordinates"
+                ] = f"""{self.forecast["city"]["coord"]["lon"]}, {self.forecast["city"]["coord"]["lat"]}"""
+            self.result["forecast"] = forecast_data
+        else:
+            self.result["forecast"] = {"msg": "No forecast available"}
+
+
+class WindUtils:
+    def degrees_to_compass(self, degrees: int) -> str:
         deg = int((degrees / 22.5) + 0.5)
         compass = [
             "north",
@@ -61,34 +122,10 @@ class Weatherman(object):
         ]
         return compass[(deg % 16)]
 
-    def utc_time_convertion(self, time):
-        utc_time = datetime.fromtimestamp(time)
-        return utc_time.strftime("%H:%M")
 
-    def get_forecast_weather_data(self):
-        forecast_list = self.forecast["list"]
-        return [
-            {
-                "temperature": f"{self.get_temperature_in_celsius(weather['main']['temp'])} °C, {self.get_temperature_in_fahrenheit(weather['main']['temp'])} °F",
-                "wind": f"{beaufort_scale.beaufort_scale_ms(self.weather['wind']['speed'], language='en')}, {self.weather['wind']['speed']} m/s, {self.degrees_to_compass(self.weather['wind']['deg'])}",
-                "cloudiness": weather["weather"][0]["description"],
-                "humidity": f"{weather['main']['humidity']}%",
-                "time": weather["dt_txt"],
-            }
-            for weather in forecast_list
-        ]
+class TempUtils:
+    def kelvin_to_celsius(self, kelvin: float) -> float:
+        return kelvin - 273.15
 
-    def get_current_weather_data(self):
-        return {
-            "location_name": f"{self.weather['name']}, {self.weather['sys']['country']}",
-            "temperature": f"{self.get_temperature_in_celsius(self.weather['main']['temp'])} °C, {self.get_temperature_in_fahrenheit(self.weather['main']['temp'])} °F",
-            "wind": f"{beaufort_scale.beaufort_scale_ms(self.weather['wind']['speed'], language='en')}, {self.weather['wind']['speed']} m/s, {self.degrees_to_compass(self.weather['wind']['deg'])}",
-            "cloudiness": self.weather["weather"][0]["description"],
-            "pressure": f"{self.weather['main']['pressure']} hPa",
-            "humidity": f"{self.weather['main']['humidity']}%",
-            "sunrise": f"{self.utc_time_convertion(self.weather['sys']['sunrise'])}",
-            "sunset": f"{self.utc_time_convertion(self.weather['sys']['sunset'])}",
-            "geo_coordinates": f"[{self.weather['coord']['lat']}, {self.weather['coord']['lon']}]",
-            "requested_time": f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",
-            "forecast": self.get_forecast_weather_data(),
-        }
+    def kelvin_to_fahrenheit(self, kelvin: float) -> float:
+        return kelvin * 1.8 - 459.67
